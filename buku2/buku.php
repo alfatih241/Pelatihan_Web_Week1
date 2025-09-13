9fcbebe875cdf605f0a90285a701806c70e599f5
Fatih <alfatih0610212@gmail.com>
2025-09-13 17:07:07 +0800

    FatihTask

---
+++ buku2/buku.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Toko Buku</title>
+    </head>
+    <body style="margin:0; font-family:Arial, sans-serif;">
+        <br>
+        <!-- Katalog Buku -->
+        <div style="display:grid; grid-template-columns:repeat(4, 1fr); gap:20px; width:90%; margin:auto;">
+
+            <!-- Buku 1 -->
+            <div style=" padding:10px; text-align:center;">
+
+                <a href="buku.jpg"></a>
+                <img src="Assets/image/buku.jpg" style="width:100%; height:300px; object-fit:cover;">
+                <h3>Buku Pintar</h3>
+                <p style="color:gray;">Edisi</p>
+                <p style="font-weight:bold;">Rp55.000</p>
+                <button style="border:1px solid black; background:white; padding:8px 15px; cursor:pointer;">Pesan Sekarang</button>
+            </div>
+
+            <!-- Buku 2 -->
+            <div style=" padding:10px; text-align:center;">
+                <div style="width:100%; height:300px; background:Lightgrey;"></div>
+                <h3>Novel Terbaik</h3>
+                <p style="color:gray;">Baru</p>
+                <p style="font-weight:bold;">Rp75.000</p>
+                <button style="border:1px solid black; background:white; padding:8px 15px; cursor:pointer;">Pesan Sekarang</button>
+            </div>
+
+            <!-- Buku 3 -->
+            <div style="padding:10px; text-align:center;">
+                 <img src="Assets/image/novel.png" style="width:100%; height:300px; object-fit:cover;"> 
+                <h3>Buku Fiksi</h3>
+                <p style="color:gray;">Klasik</p>
+                <p style="font-weight:bold;">Rp60.000</p>
+                <button style="border:1px solid black; background:white; padding:8px 15px; cursor:pointer;">Pesan Sekarang</button>
+            </div>
+
+            <!-- Buku 4 -->
+            <div style=" padding:10px; text-align:center;">
+                <div style="width:100%; height:300px; background:Lightgrey;"></div>
+                <h3>Buku Anak</h3>
+                <p style="color:gray;">Edukasi</p>
+                <p style="font-weight:bold;">Rp45.000</p>
+                <button style="border:1px solid black; background:white; padding:8px 15px; cursor:pointer;">Pesan Sekarang</button>
+            </div>
+
+            <!-- Buku 5 -->
+            <div style=" padding:10px; text-align:center;">
+                <div style="width:100%; height:300px; background:Lightgrey;"></div>
+                <h3>Buku Resep</h3>
+                <p style="color:gray;">Kreatif</p>
+                <p style="font-weight:bold;">Rp50.000</p>
+                <button style="border:1px solid black; background:white; padding:8px 15px; cursor:pointer;">Pesan Sekarang</button>
+            </div>
+
+            <!-- Buku 6 -->
+            <div style=" padding:10px; text-align:center;">
+                <div style="width:100%; height:300px; background:Lightgrey;"></div>
+                <h3>Buku Sejarah</h3>
+                <p style="color:gray;">Kuno</p>
+                <p style="font-weight:bold;">Rp70.000</p>
+                <button style="border:1px solid black; background:white; padding:8px 15px; cursor:pointer;">Pesan Sekarang</button>
+            </div>
+
+            <!-- Buku 7 -->
+            <div style=" padding:10px; text-align:center;">
+                <div style="width:100%; height:300px; background:Lightgrey;"></div>
+                <h3>Buku Teknologi</h3>
+                <p style="color:gray;">Inovasi</p>
+                <p style="font-weight:bold;">Rp80.000</p>
+                <button style="border:1px solid black; background:white; padding:8px 15px; cursor:pointer;">Pesan Sekarang</button>
+            </div>
+
+            <!-- Buku 8 -->
+            <div style=" padding:10px; text-align:center;">
+                <div style="width:100%; height:300px; background:Lightgrey;"></div>
+                <h3>Buku Motivasi</h3>
+                <p style="color:gray;">Inspiratif</p>
+                <p style="font-weight:bold;">Rp65.000</p>
+                <button style="border:1px solid black; background:white; padding:8px 15px; cursor:pointer;">Pesan Sekarang</button>
+            </div>
+
+        </div>
+
+    </body>
+</html>
